e1255c5eeacc11b2926d251cdc3030cbc996d9c4
DaoAnhVu <s3926187@rmit.edu.vn>
2022-08-22 23:19:31 +0700

    fix header and change profile image

--- www/includes/header.php
+++ www/includes/header.php
@@ -5,44 +5,44 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
-    <link rel="stylesheet" href="/www/includes/shared.css">
+    <link rel="stylesheet" href="/includes/shared.css">
 </head>
 
 <body>
     <header>
         <nav>
             <div>
-                <a href="/www/index.php">LOZODO</a>
+                <a href="/index.php">LOZODO</a>
                 <div>
                     <ul>
                         <?php if (isset($_SESSION['user-type']) && $_SESSION['user-type'] == "1"): ?>
                         <li class="nav-item">
-                            <a class="nav-link" href="/www/my-account/my-account.php">My account</a>
+                            <a class="nav-link" href="/my-account/my-account.php">My account</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/www/customer/view-product.php">View product</a>
+                            <a class="nav-link" href="/customer/view-product.php">View product</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/www/customer/cart.php">Cart</a>
+                            <a class="nav-link" href="/customer/cart.php">Cart</a>
                         </li>
                         <?php endif ;?>
                         <?php if (isset($_SESSION['user-type']) && $_SESSION['user-type'] == "2"): ?>
                         <li class="nav-item">
-                            <a class="nav-link" href="/www/my-account/my-account.php">My account</a>
+                            <a class="nav-link" href="/my-account/my-account.php">My account</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/www/vendor/add-new-product.php">Add new product</a>
+                            <a class="nav-link" href="/vendor/add-new-product.php">Add new product</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/www/vendor/view-product.php">View product</a>
+                            <a class="nav-link" href="/vendor/view-product.php">View product</a>
                         </li>
                         <?php endif ;?>
                         <?php if (isset($_SESSION['user-type']) && $_SESSION['user-type'] == "3"): ?>
                         <li class="nav-item">
-                            <a class="nav-link" href="/www/my-account/my-account.php">My account</a>
+                            <a class="nav-link" href="/my-account/my-account.php">My account</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/www/shipper/view-order.php">View orders</a>
+                            <a class="nav-link" href="/shipper/view-order.php">View orders</a>
                         </li>
                         <?php endif ;?>
 
--- www/my-account/my-account-include.php
+++ www/my-account/my-account-include.php
@@ -10,19 +10,18 @@ if (isset($_SESSION['username'])) {
     }
     fclose($readData);
 
-    foreach ($profileImagesData as $profileImage) {
-        if ($profileImage[0] == $currentUser) {
+    for ($i=0; $i < count($profileImagesData); $i++) { 
+        if ($profileImagesData[$i][0] == $currentUser) {
             $fileName = $_FILES['profile-image']['name'];
             $fileExplode = explode('.', $fileName);
             $fileExt = strtolower(end($fileExplode));;
             $newFileName = uniqid('') . "." . $fileExt;
             $fileLocation = '../profile-images/' . $newFileName;
             move_uploaded_file($_FILES['profile-image']['tmp_name'], $fileLocation);
-            $profileImage[1] = $newFileName;
+            $profileImagesData[$i][1] = $newFileName;
             $_SESSION['profile-image'] = $newFileName;
         }
-    }
-    
+    } 
 
     $writeData = fopen('../profile-images/profile-images.db', 'w');
     flock($writeData, LOCK_EX);
