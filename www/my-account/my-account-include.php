cba224ea288d2c459950c74bd0bfe3c0f84ca0e9
NgDK27 <85181668+NgDK27@users.noreply.github.com>
2022-08-23 11:08:28 +0700

    Fixed validator

--- www/my-account/my-account-include.php
+++ www/my-account/my-account-include.php
@@ -9,20 +9,27 @@ if (isset($_SESSION['username'])) {
         $profileImagesData[] = $line;
     }
     fclose($readData);
+    $changed = false;
 
-    foreach ($profileImagesData as $profileImage) {
-        if ($profileImage[0] == $currentUser) {
+    for ($i=0; $i < count($profileImagesData); $i++) { 
+        if ($profileImagesData[$i][0] == $currentUser) {
             $fileName = $_FILES['profile-image']['name'];
-            $fileExplode = explode('.', $fileName);
-            $fileExt = strtolower(end($fileExplode));;
-            $newFileName = uniqid('') . "." . $fileExt;
-            $fileLocation = '../profile-images/' . $newFileName;
-            move_uploaded_file($_FILES['profile-image']['tmp_name'], $fileLocation);
-            $profileImage[1] = $newFileName;
-            $_SESSION['profile-image'] = $newFileName;
+            if (!empty($fileName)) {
+                $fileExplode = explode('.', $fileName);
+                $fileExt = strtolower(end($fileExplode));;
+                $newFileName = uniqid('') . "." . $fileExt;
+                $fileLocation = '../profile-images/' . $newFileName;
+                move_uploaded_file($_FILES['profile-image']['tmp_name'], $fileLocation);
+                $profileImage[1] = $newFileName;
+                $profileImagesData[$i][1] = $newFileName;
+                $_SESSION['profile-image'] = $newFileName;
+                $changed = true;
+            } else {
+                    header("Location: ./my-account.php?changeppf=fail");
+            }
+        
         }
     }
-    
 
     $writeData = fopen('../profile-images/profile-images.db', 'w');
     flock($writeData, LOCK_EX);
@@ -31,7 +38,9 @@ if (isset($_SESSION['username'])) {
     }
     flock($writeData, LOCK_UN);
     fclose($writeData);
-    header("Location: ./my-account.php");
+    if ($changed) {
+        header("Location: ./my-account.php");
+    }
 } else {
     print_r($_SESSION);
     print_r($_POST);
