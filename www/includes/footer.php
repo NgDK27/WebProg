5240e55e0f4b510ed36848028c3a93db60d162dc
NathanPham19 <100595190+NathanPham19@users.noreply.github.com>
2022-08-29 00:01:17 +0700

    Added footer

--- www/includes/footer.php
+++ www/includes/footer.php
@@ -1,12 +1,34 @@
-<body>
- <footer class="footer">
-    <div class="footer-left">
-        
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<footer>
+    <!--Footer About-->
+    <div class="container">
+        <div class="footer_about">
+            <h2> About LOZODO </h2>
+            <p>Second biggest ecommerce website in Vietnam. Providing you goods in just a click!</p>
+
+            <a href="#" class="fa fa-facebook fa-lg"></a>
+            <a href="#" class="fa fa-twitter fa-lg"></a>
+            <a href="#" class="fa fa-google fa-lg"></a>
+            <a href="#" class="fa fa-instagram fa-lg"></a>
+            
+        </div>
     </div>
- </footer>
+
+    <!--Footer Links-->
+    <div class="footer_link">
+        <h2>Company</h2>
+        <ul>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">Privacy </a></li>
+                <li><a href="#">Terms of Service</a></li>
+            </ul>
+    </div>
 
 
-</body>
+</footer>
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
