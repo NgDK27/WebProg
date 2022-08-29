4373d7bf966664f20add57fd35f77a52ca729d63
NgDK27 <85181668+NgDK27@users.noreply.github.com>
2022-08-29 21:24:40 +0700

    Fixed styling

--- www/shipper/view-order.php
+++ www/shipper/view-order.php
@@ -13,30 +13,30 @@ if (!isset($_SESSION['username']) || $_SESSION['user-type'] != '3') {
 
 <div id="info-container" class="container-sm d-flex align-items-center align-self-center flex-column rounded shadow p outside-box">
 
-<?php
-$readData = fopen('../Data/order.db', 'r');
-flock($readData, LOCK_SH);
-$orderData = array();
-while ($line = fgetcsv($readData)) {
-    $orderData[] = $line;
-}
-fclose($readData);
+    <?php
+    $readData = fopen('../Data/order.db', 'r');
+    flock($readData, LOCK_SH);
+    $orderData = array();
+    while ($line = fgetcsv($readData)) {
+        $orderData[] = $line;
+    }
+    fclose($readData);
 
-foreach ($orderData as $order) {
-    if ($_SESSION["hub"] == $order[2]) {
-        if ($order[3] == "active") {
-            $id = $order[0];
-            echo "<li>";
-            echo "<a href= \"order-detail.php?id=$id\">";
-            echo "<div>";
-            print_r($id);
-            echo "</div>";
-            echo "</a>";
-            echo "</li>";
+    foreach ($orderData as $order) {
+        if ($_SESSION["hub"] == $order[2]) {
+            if ($order[3] == "active") {
+                $id = $order[0];
+                echo "<li class='mb-3'>";
+                echo "<a href= \"order-detail.php?id=$id\">";
+                echo "<div>";
+                print_r($id);
+                echo "</div>";
+                echo "</a>";
+                echo "</li>";
+            }
         }
     }
-}
-?>
+    ?>
 </div>
 
 <?php
